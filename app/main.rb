005e00fcfb9d453ca6193b882a92a5264e912fd4
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-12 00:59:43 -0300

    chore: Imports operations into the main file

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,7 @@
 require "active_record"
 require "yaml"
+require_relative "./operations/accounts/create"
+require_relative "./operations/transactions/create"
 require "money"
 
 MONEY_ROUND = Money.rounding_mode = BigDecimal::ROUND_HALF_UP
@@ -12,4 +14,6 @@ end
 
 ActiveRecord::Base.establish_connection(file_config_db["development"])
 
+perform_account_creation = Operations::Accounts::Create.call(ARGV[0])
+perform_transaction_creation = Operations::Transactions::Create.call(ARGV[1])
     "Saldo final de #{Money.new(account.balance, "BRL").format} na conta #{account.id}"
