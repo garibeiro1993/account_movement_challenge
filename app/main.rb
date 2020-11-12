5decaf9fdb9d6211cd2708d03cd94c63596bb73a
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-12 01:02:37 -0300

    feat: Return bank statement

--- app/main.rb
+++ app/main.rb
@@ -18,5 +18,10 @@ perform_account_creation = Operations::Accounts::Create.call(ARGV[0])
 perform_transaction_creation = Operations::Transactions::Create.call(ARGV[1])
 
 if perform_account_creation.success? && perform_transaction_creation.success?
+  bank_statement = Account.all.map do |account|
     "Saldo final de #{Money.new(account.balance, "BRL").format} na conta #{account.id}"
+  end
+
+  puts "\n\n\n\n\n\n\n\n\n\n\n\n"
+  puts bank_statement
 end
